515ff7b40766467a027f5c144c0123b481e51d37
nobbieboy1337 <40710395+nobbieboy1337@users.noreply.github.com>
2023-01-17 12:34:07 +0530

    Update Go.sh

--- Go.sh
+++ Go.sh
@@ -1,20 +1,39 @@
-#install go
-if [[ -z "$GOPATH" ]];then
-echo "It looks like go is not installed, would you like to install it now"
-PS3="Please select an option : "
-choices=("yes" "no")
-select choice in "${choices[@]}"; do
-        case $choice in
-                yes)
+#!/bin/bash
 
-					echo "Installing Golang"
-					wget https://dl.google.com/go/go1.14.6.linux-amd64.tar.gz
-					sudo tar -xvf go1.14.6.linux-amd64.tar.gz
-					sudo mv go /usr/local
-					export GOROOT=/usr/local/go
-					export GOPATH=$HOME/go
-					export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-					echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
-					echo 'export GOPATH=$HOME/go'	>> ~/.bash_profile			
-					echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile	
-					source ~/.bash_profile
+# Download and install Go
+wget https://dl.google.com/go/go1.19.1.linux-amd64.tar.gz
+tar -xvf go1.19.1.linux-amd64.tar.gz
+sudo mv go /usr/local
+
+# Set Go environment variables
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+# Install subfinder
+go get -u github.com/projectdiscovery/subfinder/cmd/subfinder
+
+# Install assetfinder
+go get -u github.com/tomnomnom/assetfinder
+
+# Install amass
+go get -u github.com/OWASP/Amass/v3/...
+
+# Install httpx
+go get -u github.com/projectdiscovery/httpx/cmd/httpx
+
+# Install naabu
+go get -u github.com/projectdiscovery/naabu/cmd/naabu
+
+# Install nmap
+sudo apt-get install nmap
+
+# Install ffuf
+go get -u github.com/ffuf/ffuf
+
+# Install nuclei
+go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+
+# Install nuclei templates
+https://github.com/projectdiscovery/nuclei-templates
+
